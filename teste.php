833ecf93b731127b5badcd3dbfe3e998633fcbb7
Felipe Silva Belmonte <145449476+LordBelmonte@users.noreply.github.com>
2025-08-10 17:42:00 -0300

    Exercicios
    Base de codigo em PHP

---
+++ teste.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+  $script1= "te";
+  $script2= "amo";
+  $script3= "Sarah Sebolandia";
+  
+  echo "eu $script1 $script2"."<br>";
+  echo "script3";
+
+    ?>
+
+</body>
+</html>
